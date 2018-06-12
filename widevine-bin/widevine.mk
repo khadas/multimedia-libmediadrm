c88668ab69ba74cd42d73d64728a7889b546e986
Tao Guo <tao.guo@amlogic.com>
2018-06-12 11:15:37 +0800

    libmediadrm: add prebuilts for cobalt [1/1]
    PD#168074
    
    Change-Id: Ifaaba6f5f95646364891b5fd8f408eb2bc55afee

---
+++ widevine-bin/widevine.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# widevine prebuilt
+#
+################################################################################
+
+
+WIDEVINE_BIN_VERSION = $(BR2_PACKAGE_WIDEVINE_BIN_VERSION)
+WIDEVINE_BIN_SITE = $(TOPDIR)/../multimedia/libmediadrm/widevine-bin/prebuilt-v$(WIDEVINE_BIN_VERSION)
+WIDEVINE_BIN_SITE_METHOD = local
+WIDEVINE_BIN_INSTALL_TARGET := YES
+WIDEVINE_BIN_INSTALL_STAGING := YES
+WIDEVINE_BIN_DEPENDENCIES = tdk
+
+
+define WIDEVINE_BIN_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/lib/$(BR2_ARCH)/*.so $(STAGING_DIR)/usr/lib/
+endef
+
+define WIDEVINE_BIN_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/ta/*.ta $(TARGET_DIR)/lib/teetz/
+	$(INSTALL) -D -m 0644 $(@D)/lib/$(BR2_ARCH)/*.so $(TARGET_DIR)/usr/lib/
+endef
+
+$(eval $(generic-package))
