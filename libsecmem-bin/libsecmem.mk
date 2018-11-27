c60eb52263d251526fac8edb685df235e2888ac1
baoguo.chen <baoguo.chen@amlogic.com>
2018-11-27 15:03:16 +0800

    libmediadrm: update DRM files [1/1]
    PD#SWPL-1586
    
    Problem:
    move widevine ce-cdm to libmediadrm repository.
    
    Solution:
    1.change directory structure.
    2.delete ffmpeg2-bin dir.
    3.add choices for widevine-bin version.
    4.add wv14 so in widevine-bin prebuild.
    
    Verify:
    local
    
    Change-Id: I7eecdd2a2aeb29c1b3c9df15ca3ee2e0704985c8
    Signed-off-by: baoguo.chen <baoguo.chen@amlogic.com>

--- libsecmem-bin/libsecmem.mk
+++ libsecmem-bin/libsecmem.mk
@@ -14,13 +14,13 @@ LIBSECMEM_BIN_DEPENDENCIES = tdk
 
 
 define LIBSECMEM_BIN_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/lib/$(BR2_ARCH)/*.so $(STAGING_DIR)/usr/lib/
+	$(INSTALL) -D -m 0644 $(@D)/$(BR2_ARCH).$(CC_TARGET_ABI_).$(CC_TARGET_FLOAT_ABI_)/*.so $(STAGING_DIR)/usr/lib/
 endef
 
 define LIBSECMEM_BIN_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/ta/*.ta $(TARGET_DIR)/lib/teetz/
-	$(INSTALL) -D -m 0644 $(@D)/lib/$(BR2_ARCH)/*.so $(TARGET_DIR)/usr/lib/
-        $(INSTALL) -D -m 0755 $(@D)/test/$(BR2_ARCH)/* $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0644 $(@D)/noarch/ta/*.ta $(TARGET_DIR)/lib/teetz/
+	$(INSTALL) -D -m 0644 $(@D)/$(BR2_ARCH).$(CC_TARGET_ABI_).$(CC_TARGET_FLOAT_ABI_)/*.so $(TARGET_DIR)/usr/lib/
+        $(INSTALL) -D -m 0755 $(@D)/$(BR2_ARCH).$(CC_TARGET_ABI_).$(CC_TARGET_FLOAT_ABI_)/secmem_test $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
