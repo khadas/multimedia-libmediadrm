2e7522407460167fb5c1d9bfcc4dcb969a9749dd
Kihun Lee <kihun.lee@amlogic.com>
2023-05-10 18:05:17 +0800

    prebuilt: update netflix ta from Buildroot_DRM_PatchBuild-1654 [1/1]
    PD#OTT-43218
    
    Problem:
    Update prebuilt files
    
    Solution:
    None
    
    Verify:
    None
    
    Build Tag:
    Buildroot_DRM_PatchBuild-1654
    
    Commits:
    buildroot/package/amlogic/linuxdrm/netflix-ta/src:
    version: 1.2.2-g954f638(build:1654)
    954f638 esn provision : add esn provision library build [1/1]
    
    Git Hash-Object:
    93a1e29c44598457455ebfe3a790812cce69cf36
    netflix_ta-bin/prebuilt/aarch64.lp64./esn_provision
    7473f61d280c0b554b45604b71329c8013feeda4
    netflix_ta-bin/prebuilt/aarch64.lp64./libnfx_esn.so
    6eadc52ab3629701c6ace2afd81dc223b94ad756
    netflix_ta-bin/prebuilt/arm.aapcs-linux.hard/esn_provision
    6df2cb873c6d78a1b893f17f6ec3883467a9f06b
    netflix_ta-bin/prebuilt/arm.aapcs-linux.hard/libnfx_esn.so
    7793e78b13bb6bd2260ff6c57abdc44de41f50e5
    netflix_ta-bin/prebuilt/noarch/include/esn_provision_api.h
    250d00eeb03245f1505b5e1815e83014feddd559
    netflix_ta-bin/prebuilt/noarch/ta/v2.4/af1ae3a4-888b-4e11-b4ab1c2b972d1c11.ta
    a4118451a49f487ce1e390317c83ddf41516482e
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    52a1326167156cb55a9f0ea39bbbc5c2c0ef8050
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/A311D2/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    82b8630b046b365f7cf3b44af4f6b4bd2e4c28de
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/A311D2J/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    560cdf5b9ea6bdf3a5f87959577db0056bceef0a
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/POP1/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    c4b6a98a28582f6fe668ef6a705a332d26f16ba2
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S805C3L/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    6399aa273aef9af8d14d18d8914f3d26b5abd988
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S805X2/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    e6e15c10ef9b8713452dfec104676e347a36bce5
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S805X2G/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    b56a115c90fe9f492b20d877be123e5f78b55582
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905C2/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    751c09eb0221c85c8f8cbb1e7479f7ed1dc4c352
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905C2ENG/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    ffc98cf1c5e65b9c814faf807cb11e6a78a250cf
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905C2L/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    4e95e66deb3da78b90329c72c3d79ea41b70dc10
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905C3/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    5a98c1aa6a025c85ad299d6b0c7b8480266fee9a
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905C3ENG/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    35335c41640d515cdf5da08c7ec29723f5640e2d
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905C3NMA/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    5e79f63f5b0fc6d6d650d1de065daca32d4d5fea
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905W2/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    7a430e01767e82e42383c5189d4ac9aaf86eb00e
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905X4/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    5f7d051998ce6746af7b5fe7b40d096386ed141f
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/S905Y4/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    ec98e4b41da2e3dcb3e605883e6be287d53bbbcb
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/T962D4/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    f2a1ecdeefe2af274a3c14cf2dd146f9d78fdc3c
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/T963D4/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    2406081dc72c305107610d3eaa680081432d3a1f
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/T965D4/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    cdbcac9609ed78f180603183fc372de7881ad43d
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/T982/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    7f17e84db0c9da102a0873de6ca686f38feef2e6
    netflix_ta-bin/prebuilt/noarch/ta/v3.8/dev/V918D/af1ae3a4-888b-4e11-b4ab-1c2b972d1c11.ta
    
    Change-Id: I3aec5b1a070290a29de702b6e518554c99e6f2a0
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

---
+++ netflix_ta-bin/prebuilt/noarch/include/esn_provision_api.h
@@ -0,0 +1,28 @@
+/*
+ * ESN key provsioning API
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ESN_PROVISION_API_H_
+#define _ESN_PROVISION_API_H_
+
+#define ESN_API_SUCCESS					0
+#define ESN_API_FAIL					-1
+
+#define PROVISION_KEY_CHECKSUM_LENGTH                  	32
+#define PROVOSION_KEY_MAX_ENSID				42
+
+int esnkeybox_provision(char *file);
+int esnkeybox_esnid_get(uint8_t* esnid);
+int esnkeybox_esn_remove(void);
+int esnkeybox_checksum(uint8_t* checksum);
+int esnkeybox_query(uint32_t* key_size);
+
+#endif /* _ESN_PROVISION_API_H_ */
+
+#ifdef __cplusplus
+}
+#endif
